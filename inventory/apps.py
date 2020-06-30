58b9f8390b51a817042af2e34a61940616ec55f6
Jaap Joris Vens <jj@rtts.eu>
2020-06-30 17:10:10 +0200

    Dutch translations

---
+++ inventory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+class InventoryConfig(AppConfig):
+    name = 'inventory'
+    verbose_name = _('Inventory management')
